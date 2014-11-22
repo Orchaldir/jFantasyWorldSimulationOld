4c4b1a95512b6ca27f5c5b42f12787fa7755676f
Orchaldir <orchaldir@gmail.com>
2014-11-22 18:40:49 +0100

    Added project CombatPrototype.

---
+++ CombatPrototype/src/jfws/cp/CombatPrototype.java
@@ -0,0 +1,14 @@
+package jfws.cp;
+
+public class CombatPrototype
+{
+	
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Combat Prototype");
+	}
+	
+}
